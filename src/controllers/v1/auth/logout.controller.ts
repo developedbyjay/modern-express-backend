6c71f5aae5a9ba6baecdc4df4583aeb4c51875fb
developedbyjay <okegbemijoshua7@gmail.com>
2025-08-29 11:48:56 +0100

    add global express namespace

--- src/controllers/v1/auth/logout.controller.ts
+++ src/controllers/v1/auth/logout.controller.ts
@@ -2,13 +2,30 @@ import { logger } from '@src/lib/winston';
 import tokenModel from '@src/models/token.model';
 import type { Request, Response } from 'express';
 
-
-
 const logout = async (req: Request, res: Response): Promise<void> => {
- 
-    try {
-      res.sendStatus(204)
-   
+  try {
+    const refreshToken = req.cookies.refreshToken as string;
+
+    if (refreshToken) {
+      await tokenModel.deleteOne({ token: refreshToken });
+      logger.info('Refresh token deleted from database', {
+        userId: req.userId,
+        refreshToken,
+      });
+    }
+
+    res.clearCookie('refreshToken', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
+
+    logger.info('User logged out successfully', {
+      userId: req.userId,
+      refreshToken,
+    });
+
+    res.sendStatus(204);
   } catch (err) {
     res.status(500).json({
       code: 'ServerError',
--- src/utils/index.util.ts
+++ src/utils/index.util.ts
@@ -1,6 +1,5 @@
 export const generateUsername = (): string => {
   const usernamePrefix = 'user-';
-  const randomChars = Math.random().toString(36).slice(2); 
-  const username = usernamePrefix + randomChars;
-  return username;
+  const randomChars = Math.random().toString(36).slice(2);
+  return usernamePrefix + randomChars;
 };
